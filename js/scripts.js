489023886604eb1089d3bf6636b196f994e41159
Nate Lee <nleepercussion@gmail.com>
2021-06-02 09:06:05 -0400

    Wednesday morning updates

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
 //Create elements for the select items
 const dropdown1 = document.querySelector('#dropdown1');
 const button1 = document.querySelector('#button1');
-const dropdown2 = document.querySelector('#dropdown2');
-const button2 = document.querySelector('#button2');
-const dropdown3 = document.querySelector('#dropdown3');
-const button3 = document.querySelector('#button3');
-const dropdown4 = document.querySelector('#dropdown4');
-const button4 = document.querySelector('#button4');
-const dropdown5 = document.querySelector('#dropdown5');
-const button5 = document.querySelector('#button5');
-const characterGuess = document.querySelector('#characterGuess')
-const characterGuessButton = document.querySelector('#submitGuess')
 
-const resultsList = document.querySelector('#resultsList')
+const characterGuess = document.querySelector('#characterGuess');
+const characterGuessButton = document.querySelector('#submitGuess');
+const outofQuestions = document.querySelector('#outOfQuestions');
+const resultsList = document.querySelector('#resultsList');
 
 //Create empty character array
-const characterArray = []
+const characterArray = [];
 
 //Create question list
 const questionArray = [
@@ -32,63 +25,7 @@ const questionArray = [
     ["What color is your skin?", "skin_color"],
     ["What species are you?", "species"],
     ["Do you have any starships?", "starships"]
-]
-
-//Populate first dropdown menu
-function updateQuestionDropdown (filteredArray, chosenCharacter) {
-    questionArray.forEach(question => {
-        const questionOption = document.createElement('option');
-        questionOption.value = question[0];
-        questionOption.text = question[0];
-        dropdown1.appendChild(questionOption);
-    })
-    listenForQuestions(filteredArray, chosenCharacter)
-}
-
-//Listen for user choice on question1, find answer and add to DOM
-function listenForQuestions(filteredArray, chosenCharacter) {
-    button1.addEventListener('click', function () {
-        let selectedIndex = dropdown1.selectedIndex -1
-        console.log(selectedIndex)
-        console.log(questionArray[selectedIndex][1])
-        let answerPrompt = questionArray[selectedIndex][1]
-        console.log(chosenCharacter[answerPrompt])
-        const answer1 = document.createElement('li')
-        answer1.innerText = `My ${answerPrompt} is ${chosenCharacter[answerPrompt]}`
-        resultsList.appendChild(answer1)
-    })
-}
-
-
-//Populate final dropdown menu
-function updateDropdown (filteredArray, chosenCharacter) {
-    filteredArray.forEach(item => {
-        const characterGuessOption = document.createElement('option')
-        characterGuessOption.value = item.name;
-        characterGuessOption.text = item.name;
-        characterGuess.appendChild(characterGuessOption);
-    })
-    updateQuestionDropdown(filteredArray, chosenCharacter)
-}
-
-//Listen for character guesses, evaluate, and update count
-const guessCountElement = document.querySelector('#guessCount');
-let guessesRemaining = 5;
-guessCountElement.innerHTML = guessesRemaining;
-characterGuessButton.addEventListener('click', function() {
-    let userGuess = characterGuess.value
-    let chosenCharacter = 'Luke Skywalker'
-    if (userGuess === chosenCharacter) {
-        console.log("You got it!");
-    } else {
-        console.log("Not quite");
-        guessesRemaining -= 1;
-        if (guessesRemaining == 0) {
-            console.log("Link to failure screen")
-        }
-        guessCountElement.innerHTML = guessesRemaining;
-    }   
-})
+];
 
 //Fetch from API and call createCharacterArray function
 function getCharacterList () {
@@ -136,8 +73,77 @@ function filterArray (characterArray) {
 function chooseRandomCharacter (filteredArray) {
     let randomNumber = Math.floor(Math.random()* 23)
     var chosenCharacter = filteredArray[randomNumber]
-    console.log(chosenCharacter.name)
+    console.log("Random character is ", chosenCharacter.name)
     updateDropdown(filteredArray, chosenCharacter)
+}
+
+//Populate final dropdown menu
+function updateDropdown (filteredArray, chosenCharacter) {
+    filteredArray.forEach(item => {
+        const characterGuessOption = document.createElement('option')
+        characterGuessOption.value = item.name;
+        characterGuessOption.text = item.name;
+        characterGuess.appendChild(characterGuessOption);
+    })
+    updateQuestionDropdown(filteredArray, chosenCharacter)
+}
+
+//Populate first dropdown menu
+function updateQuestionDropdown (filteredArray, chosenCharacter) {
+    questionArray.forEach(question => {
+        const questionOption = document.createElement('option');
+        questionOption.value = question[0];
+        questionOption.text = question[0];
+        dropdown1.appendChild(questionOption);
+    })
+    listenForQuestions(filteredArray, chosenCharacter);
+    listenForGuesses(filteredArray, chosenCharacter);
+};
+
+//Listen for user choice on question1, find answer and add to DOM
+function listenForQuestions(filteredArray, chosenCharacter) {
+    const questionCountElement = document.querySelector('#questionCount');
+    let questionsRemaining = 5;
+    questionCountElement.innerHTML = questionsRemaining;
+    
+    button1.addEventListener('click', function () {
+        let selectedIndex = dropdown1.selectedIndex -1
+        console.log(selectedIndex)
+        console.log(questionArray[selectedIndex][1])
+        let answerPrompt = questionArray[selectedIndex][1]
+        console.log(chosenCharacter[answerPrompt])
+        const answer1 = document.createElement('li')
+        answer1.innerText = `My ${answerPrompt} is ${chosenCharacter[answerPrompt]}`
+        resultsList.appendChild(answer1)
+        questionsRemaining -= 1;
+        if (questionsRemaining == 0) {
+            outofQuestions.innerHTML = "You are out of questions. Please make a guess."
+            question1.style.visibility = "collapse";
+        }
+        questionCountElement.innerHTML = questionsRemaining;
+    })
+}
+
+//Listen for character guesses, evaluate, and update count
+function listenForGuesses (filteredArray, chosenCharacter) {
+    const guessCountElement = document.querySelector('#guessCount');
+    let guessesRemaining = 5;
+    guessCountElement.innerHTML = guessesRemaining;
+    characterGuessButton.addEventListener('click', function() {
+        let userGuess = characterGuess.value;
+        console.log("User guessed: ", userGuess);
+        if (userGuess === chosenCharacter.name) {
+            console.log("You got it!");
+            //link to success screen
+        } else {
+            console.log("Not quite");
+            guessesRemaining -= 1;
+            if (guessesRemaining == 0) {
+                console.log("Link to failure screen")
+            }
+            guessCountElement.innerHTML = guessesRemaining;
+        }   
+    })
 }
 
 getCharacterList()
